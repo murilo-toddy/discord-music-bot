bc73adc4154f7790719e2f6679b1349f9e7c6f09
murilo-toddy <murilo.todao@usp.br>
2021-09-17 21:17:25 -0300

    Criação molde comandos

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 import discord
-import os
+from discord.ext import commands
+import os, discord
+from discord.ext import commands
+from dotenv import load_dotenv
 
-client = discord.Client()
-token = os.getenv("TOKEN")
+load_dotenv()
+
+client = commands.Bot(command_prefix="!")
+token = 'ODg4NTY4MjA4ODI1NjQ3MTA0.YUUloQ.2KrEmBYH6wp3E-6pfk14y9KWbKY'
 
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith("$hello"):
-        await message.channel.send("yes")
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return   
+#
+#     if message.content.startswith("!hello"):
+#         await message.channel.send("yes")
 
-client.run(os.getenv(token))
+voice = discord.VoiceChannel
+
+# @client.command(name="play")
+# async def play(ctx):
+#     voiceChannel = ctx.author.voice.channel
+#     voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+#     # voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+#     await voice.move_to(channel)
+
+@client.command(name="join")
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    voice = get(self.bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()    
+
+
+# print (os.getenv('TOKEN'))
+client.run(os.getenv('TOKEN'))
 
 
