8be41d0b5aa8d32886484f603abc8d4fbca89b0b
murilo-toddy <murilo.todao@usp.br>
2021-09-27 23:49:11 -0300

    Adds Spotify credentials as path variables

--- spotify.py
+++ spotify.py
@@ -5,10 +5,18 @@ from dotenv import load_dotenv
 
 async def get_spotify_info(url, ctx, queue):
 
-    load_dotenv()
+    if os.path.isfile("./.env"):
+        load_dotenv()
+        CLIENT_ID = os.getenv("SPOTIFY_ID")
+        CLIENT_SECRET = os.getenv("SPOTIFY_SECRET")
+
+    else:
+        CLIENT_ID = os.environ['SPOTIFY_ID']
+        CLIENT_SECRET = os.environ['SPOTIFY_SECRET']
+    
     spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-        client_id=os.getenv("SPOTIFY_ID"),
-        client_secret=os.getenv("SPOTIFY_SECRET")
+        client_id = CLIENT_ID,
+        client_secret = CLIENT_SECRET
     ))
 
     if url.find("playlist", 25, 35) != -1:
