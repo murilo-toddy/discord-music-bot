2bd29960e59f3bc71155bb5b8a54e7e84fed99ed
murilo-toddy <murilo.todao@usp.br>
2021-10-03 00:08:00 -0300

    Version 1.001

--- commands/help.py
+++ commands/help.py
@@ -25,7 +25,7 @@ def get_description(args):
         description += "`help`, `clear`, `forceskip`, `join`, `leave`\n"
         description += "`loop`, `loopqueue`, `move`, `nowplaying`\n"
         description += "`pause`, `play`, `queue`, `remove`, `resume`\n"
-        description += "`seek`, `shuffle`\n"
+        description += "`seek`, `shuffle`, `credits`\n"
 
         return description
 
