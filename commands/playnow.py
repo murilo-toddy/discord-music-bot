5684daf9326e1f64d54bd2acf5e8f65f1d1af1f2
EduardoCavValenca <eduardo.cvalenca@gmail.com>
2021-10-25 21:52:41 -0300

    Fixing message video at query

--- commands/playnow.py
+++ commands/playnow.py
@@ -4,6 +4,6 @@ async def play_now(client, ctx, queue, bot_info, counter, *url):
     size = len(queue)
     await cmd.play.play(client, ctx, queue, bot_info, counter, *url)
     if len(queue) > 1:
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
         if size < len(queue):
             await cmd.move.move_to(ctx, queue, size, 1)
--- commands/playskip.py
+++ commands/playskip.py
@@ -5,12 +5,12 @@ async def play_skip(client, ctx, queue, bot_info, counter, *url):
     size = len(queue)
     await cmd.play.play(client, ctx, queue, bot_info, counter, *url)
     if len(queue) > 1:
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
         if size < len(queue):
             await cmd.move.move_to(ctx, queue, size, 1)
             await force_skip(client,ctx,queue,bot_info)
     if len(queue) == 1:
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
         await force_skip(client,ctx,queue,bot_info)
     
         
