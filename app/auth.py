ff00b75e5a7b9c70e43f14359e09d95bb998ef2f
murilo_toddy <murilo.todao@gmail.com>
2023-07-15 21:42:41 -0300

    create authentication dictionary

--- app/auth.py
+++ app/auth.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 if os.path.isfile(".env"):
     load_dotenv()
 
-auth = {key: value for key, value in os.environ.items()}
+authentication = {key: value for key, value in os.environ.items()}
 
