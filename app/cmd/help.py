09f2e906b3d3550b4765d75bd7ce6c73867ff7f7
murilo_toddy <murilo.todao@gmail.com>
2023-07-17 10:24:48 -0300

    finish help command implementation

--- app/cmd/help.py
+++ app/cmd/help.py
@@ -6,7 +6,20 @@ from data.worker import Worker
 async def help_function(worker: Worker, ctx: Context, *args):
     commands_dict = worker.commands.alias_to_command
     if not args or len(args) > 1 or args[0].lower() in commands_dict["help"].aliases:
-        await worker.send_embed_message(ctx, "ajuda", "desc")
+        available_commands = worker.commands.get_all_commands()
+        commands_per_line = 4
+        available_commands_formatted = " ".join([
+                f"`{cmd}`,\n" if (i + 1) % commands_per_line == 0 else f"`{cmd}`, " \
+                for i, cmd in enumerate(available_commands)
+            ])
+
+        description = (
+            f"_Use_ `!help <cmd>` _to get help for a\nspecific command_\n\n"
+            f"**Available Commands:**\n"
+            # escape last comma
+            f"{available_commands_formatted[:-2]}\n"
+        )
+        await worker.send_embed_message(ctx, "**Help Command**", description)
         return
 
     cmd = args[0].lower()
