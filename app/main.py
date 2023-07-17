90835a45db21d49ae3919320e1ee285fee3c8453
murilo_toddy <murilo.todao@gmail.com>
2023-07-17 14:11:27 -0300

    add server creation on startup

--- app/main.py
+++ app/main.py
@@ -7,10 +7,9 @@ from data.worker import Worker
 from cmd import commands
 
 
-
-command_prefix = "!"
+COMMAND_PREFIX = "!"
 instance = Bot(
-        command_prefix=command_prefix,
+        command_prefix=COMMAND_PREFIX,
         case_insensitive=True,
         help_command=None,
         intents=discord.Intents.all(),
@@ -22,7 +21,7 @@ worker = Worker(commands)
 @instance.command(aliases=commands.get_all_aliases())
 async def function_handler(ctx: Context, *args):
     # ignore command_prefix
-    command, *args = ctx.message.content.split(command_prefix)[1].split(" ")
+    command, *args = ctx.message.content.split(COMMAND_PREFIX)[1].split(" ")
     logger.log_command(ctx, command, *args)
     await commands.execute_command(command, worker, ctx, *args)
 
@@ -34,6 +33,8 @@ async def on_ready():
         activity=discord.Activity(type=discord.ActivityType.listening, name="!help")
     )
     logger.log.info("Bot status updated successfully")
+    worker.load(instance.guilds)
+    logger.log.info("Successfully created all servers")
 
 
 if __name__ == "__main__":
